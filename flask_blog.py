50c7110545c917f2f49efcb6a8f3695212b14960
Awalu-Levison <levisonawalu251@gmail.com>
2024-08-21 18:48:42 +0200

    Add and Update some files

--- flask_blog.py
+++ flask_blog.py
@@ -1,7 +1,8 @@
-from crypt import methods
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash, redirect
 from forms import RegistrationForm, LoginForm
 app = Flask(__name__)
+
+"""Set up secret key to avoid cross site scripting attacks"""
 app.config['SECRET_KEY'] = '82f3b2a95b230b4941abe57a985edebc'
 
 
@@ -33,6 +34,9 @@ def about():
 @app.route("/register", methods=['GET', 'POST'])
 def register():
     form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
     return render_template('register.html', title='Register', form=form)
 
 @app.route("/login")
